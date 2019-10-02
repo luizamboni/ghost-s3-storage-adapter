5973c81ebbd737e39dc2504aac36784c1bc91b56
Luiz Carlos Zamboni <luiz.zamboni@zoom.com.br>
2019-10-02 15:15:24 -0300

    ADD BaseStorage to external config

--- webpack.config.js
+++ webpack.config.js
@@ -2,12 +2,29 @@
 const path = require('path')
 const { version } = require('./package.json')
 
-module.exports = {
-    entry: './index.js',
-    target: 'node',
-    output: {
-        path: path.resolve(__dirname, 'dist', version),
-        filename: 's3.js',
-        libraryTarget: 'umd'
-    }
-}
+module.exports = [ 
+    {
+        entry: './index.js',
+        target: 'node',
+        output: {
+            path: path.resolve(__dirname, 'dist', version),
+            filename: 's3.js',
+            libraryTarget: 'umd'
+        },
+        externals: {
+            'ghost-storage-base' : 'ghost-storage-base'
+        },
+    },
+    {
+        entry: './index.js',
+        target: 'node',
+        output: {
+            path: path.resolve(__dirname, 'dist', version),
+            filename: 's3.debug.js',
+            libraryTarget: 'umd'
+        },
+        optimization: {
+            minimize: false
+        },
+    },
+]
